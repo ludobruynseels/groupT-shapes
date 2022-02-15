4b564bcf3e67fec02558a4bfb13dc355e7567fc1
betelgeuze <ludo.bruynseels@gmail.com>
2022-02-15 15:23:04 +0100

    working 'Shapes' project

---
+++ src/Lab1/groept/be/Main.java
@@ -0,0 +1,19 @@
+package Lab1.groept.be;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Circle c = new Circle();
+        c.makeVisible();
+
+        Square s = new Square();
+        s.makeVisible();
+
+        Triangle t = new Triangle();
+        t.makeVisible();
+
+        Person p = new Person();
+        p.makeVisible();
+    }
+}
